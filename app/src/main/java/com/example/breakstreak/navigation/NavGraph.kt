a9f4c3d6fc7df8ffb6eb954b57ce60078290b0bf
andreeav19 <andra_368@yahoo.com>
2025-05-28 18:22:19 +0300

    add login + register screens

---
+++ app/src/main/java/com/example/breakstreak/navigation/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.breakstreak.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.breakstreak.ui.login.LoginScreen
+import com.example.breakstreak.ui.register.RegisterScreen
+
+sealed class Screen(val route: String) {
+    object Login : Screen("login")
+    object Register : Screen("register")
+    object Home : Screen("home")
+}
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Login.route) {
+        composable(Screen.Login.route) {
+            LoginScreen(onNavigateToRegister = { navController.navigate(Screen.Register.route) })
+        }
+        composable(Screen.Register.route) {
+            RegisterScreen(onRegisterSuccess = { navController.navigate(Screen.Login.route) })
+        }
+        composable(Screen.Home.route) {
+//            HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/breakstreak/ui/login/LoginScreen.kt
@@ -0,0 +1,6 @@
+package com.example.breakstreak.ui.login
+
+class LoginScreen (
+    onNavigateToRegister: () -> Unit = {}
+) {
+}
